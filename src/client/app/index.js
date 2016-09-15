7b4d4dae3f91f4b622c21d8816449ae48a008856
JamesGotsell <j.e.gotsell@gmail.com>
2016-09-15 21:47:23 +0100

    created basic component

--- src/client/app/index.js
+++ src/client/app/index.js
@@ -1,1 +1,16 @@
-console.log('boom')
+import React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+          <h1> stuff</h1>
+          <p>basic app component</p>
+      </div>
+    )
+  }
+}
+
+render(<App/>, document.getElementById('app'));
